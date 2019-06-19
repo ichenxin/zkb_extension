945c1f877be5e239d54ad154c7167310462d9c6b
krapnik <krapnik@qq.com>
2019-06-19 14:57:49 +0800

    优化定时提交器

--- js/submitter.js
+++ js/submitter.js
@@ -1,8 +1,9 @@
 window.onload = function () {
     var form = layui.form,
         t1 = null,//获取北京的定时器
-        t2 = null,//倒计时的定时器
         t3 = null,//提交的定时器;
+        span = null,
+        temp = null,
         self = null,
         date = null,
         now = null;
@@ -19,7 +20,7 @@ window.onload = function () {
         el: '.layui-layout',
         data: {
             now: {//当前北京时间
-                string: '1999-01-01 00:00:00:00”',
+                string: '1999-01-01 00:00:00:000',
                 timestamp: 0,//时间戳
                 year: null,
                 month: null,
@@ -30,13 +31,16 @@ window.onload = function () {
                 millisecond: null
             },
             post_span: '500',//提交间隔
-            post_url: 'http://m.baidu.com/',//提交地址
+            post_url: 'https://quan.suning.com/getSysTime.do',//提交地址
             post_url_temp: null,//缓存提交地址
             post_count: 0,//提交数次
             post_window_count: 1,//提交窗口
+            post_mode:1,//提交模式
             is_post: false,//是否正在提交
+            post_res:"",//请求返回信息
             post_window_arr: [],
             has_post_count: 0,//提交次数
+            first_commit_time:"",
             count_down: {  //倒计时
                 string: '1999-01-01 00:00:00',
                 day: 0,
@@ -54,7 +58,8 @@ window.onload = function () {
                 second: 0,
                 millisecond: 0,
                 temp: 0
-            }
+            },
+            is_post_trigger:false
         },
         methods: {
             getTime: function () {//获取北京时间
@@ -76,7 +81,7 @@ window.onload = function () {
             timer: function () {//获取北京时间的定时器
                 t1 = setInterval(function () {
                     self.getTime();
-                    if(self.is_post){
+                    if(self.is_post && !self.is_post_trigger){
                         self.countDown();
                     }
                 }, 500)
@@ -94,58 +99,67 @@ window.onload = function () {
                 this.count_down.string = String(this.post_time.year) + "/" + String(this.post_time.month) + "/" + String(this.post_time.day) + " " + String(this.post_time.hour) + ":" + String(this.post_time.minutes) + ":" + String(this.post_time.second);  //拼接时间戳字符
                 this.count_down.string = new Date(this.count_down.string).getTime() + Number(this.post_time.millisecond);
                 this.is_post = true;
-                // this.countDown();
+                if(self.post_mode !== 1){
+                    self.getCommit();
+                }
             },
             cancel: function () {
                 this.stop();
+                this.first_commit_time = "";
                 this.is_post = false;
                 this.post_url = this.post_url_temp;
                 this.has_post_count = 0;
+                this.is_post_trigger = false;
             },
             countDown: function () {//倒计时
-                // var self = this;
-                // t2 = window.setInterval(function () {
-                var t = self.count_down.string - self.now.timestamp;
-                self.count_down.day = Math.floor(t / 1000 / 60 / 60 / 24);
-                self.count_down.hour = Math.floor(t / 1000 / 60 / 60 % 24);
-                self.count_down.minutes = Math.floor(t / 1000 / 60 % 60);
-                self.count_down.second = Math.floor(t / 1000 % 60);
-                var temp = "" + t;
-                self.count_down.millisecond = Math.floor(t > 1000 ? temp.substr(temp.length - 3, temp.length) : temp);
-                if (t <= 0) {
+                span = self.count_down.string - self.now.timestamp;
+                self.count_down.day = Math.floor(span / 1000 / 60 / 60 / 24);
+                self.count_down.hour = Math.floor(span / 1000 / 60 / 60 % 24);
+                self.count_down.minutes = Math.floor(span / 1000 / 60 % 60);
+                self.count_down.second = Math.floor(span / 1000 % 60);
+                temp = "" + span;
+                self.count_down.millisecond = span > 1000 ? "" + temp.substr(temp.length - 3, temp.length) : temp;
+                if (span <= 0) {
+                    self.commit();
                     self.count_down.day = 0;
                     self.count_down.hour = 0;
                     self.count_down.minutes = 0;
                     self.count_down.second = 0;
                     self.count_down.millisecond = 0;
-                    self.commit();
+                    self.first_commit_time = self.now.string;
+                    self.is_post_trigger = true;
                 }
-                // }, 500);
-
             },
             commit: function () {//提交
-                // window.clearInterval(t2);
-                // t2 = null;
-                // var self = this;
                 t3 = setInterval(function () {
                     //次数
                     self.has_post_count += Number(self.post_window_count);
-                    //刷新提交地址
-                    if (self.post_url.indexOf('?') > -1) {
-                        self.post_url = self.post_url_temp + "&t=" + self.has_post_count;
-                    } else {
-                        self.post_url = self.post_url_temp + "?t=" + self.has_post_count;
+                    if(self.post_mode == 1){
+                        self.frameCommit();
+                    }else{
+                        self.getCommit();
                     }
                     if (self.has_post_count >= self.post_count && Number(self.post_count) !== 0) {
                         self.stop();
                     }
                 }, self.post_span);
             },
+            frameCommit:function(){
+                //刷新提交地址
+                if (self.post_url.indexOf('?') > -1) {
+                    self.post_url = self.post_url_temp + "&t=" + self.has_post_count;
+                } else {
+                    self.post_url = self.post_url_temp + "?t=" + self.has_post_count;
+                }
+            },
+            getCommit:function(){
+                fetch(self.post_url).then(function (res) {
+                    return res.json();
+                }).then(function (value) {
+                    self.post_res = value;
+                })
+            },
             stop: function () {//停止提交
-                // if (t2) {
-                //     window.clearInterval(t2);
-                //     t2 = null;
-                // }
                 if (t3) {
                     window.clearInterval(t3);
                     t3 = null;
